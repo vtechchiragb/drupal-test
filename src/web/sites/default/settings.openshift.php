cebec71814ed3917797729004f3c7c547bae790e
vtechchiragb <39927825+vtechchiragb@users.noreply.github.com>
2024-03-28 09:23:47 -0400

    Update settings.openshift.php

--- src/web/sites/default/settings.openshift.php
+++ src/web/sites/default/settings.openshift.php
@@ -3,12 +3,15 @@ $databases['default']['default'] = [
 'database' => getenv('MYSQL_DB'),
 'username' => getenv('MYSQL_USER'),
 'password' => getenv('MYSQL_PASSWORD'),
-'host' => getenv('MYSQL_HOST'),
-'port' => getenv('MYSQL_POST'),
-'driver' => 'mysql',
-'prefix' => ''
+'host' => getenv('POSTGRES_HOST'),
+'port' => getenv('POSTGRES_POST'),
+'driver' => 'pgsql',
+'prefix' => '',
+'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
+'autoload' => 'core/modules/pgsql/src/Driver/Database/pgsql/',
 ];
 $settings['hash_salt'] = json_encode($databases);
+
 
 ini_set('xdebug.max_nesting_level', 500);
 ini_set('memory_limit', '128M');
